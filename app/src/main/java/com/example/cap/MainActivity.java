e003faf1658be525c203768f3d2374f0c8108578
s-jis <s-jis@naver.com>
2019-05-23 19:40:18 +0900

    Toolbar 틀

--- app/src/main/java/com/example/cap/MainActivity.java
+++ app/src/main/java/com/example/cap/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.cap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //주석
-        //fdslkdsfljfsd
-        ///sfdksdnls
-        //하하하하하
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  // 왼쪽 버튼 사용 여부 true
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu);  // 왼쪽 버튼 이미지 설정
+        getSupportActionBar().setDisplayShowTitleEnabled(false);    // 타이틀 안보이게 하기
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
